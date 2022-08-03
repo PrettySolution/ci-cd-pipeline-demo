228584ab23066eade881d24c347585da0cb76a01
VH🔥 <vherman@vf-soft.com>
2022-08-03 22:52:58 +0300

    angular

--- lib/demo-pipeline-stack.ts
+++ lib/demo-pipeline-stack.ts
@@ -12,7 +12,7 @@ export class DemoPipelineStack extends Stack {
       pipelineName: 'demo-pipeline',
       synth: new ShellStep('Synth', {
         input: CodePipelineSource.gitHub('PrettySolution/ci-cd-pipeline-demo', 'main'),
-        commands: ['npm ci', 'npm run build', 'npx cdk synth', 'ls -la'],
+        commands: ['ls -la', 'npm ci', 'npm run build', 'npx cdk synth'],
         additionalInputs: {
           './angular': CodePipelineSource.gitHub('PrettySolution/ci-cd-fe-demo', 'main', {
             trigger: GitHubTrigger.WEBHOOK
