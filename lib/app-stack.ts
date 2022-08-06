eee8de4f960d53c910444c02562092c3bd068a61
VH🔥 <vherman@vf-soft.com>
2022-08-06 15:44:10 +0300

    add database

--- lib/app-stack.ts
+++ lib/app-stack.ts
@@ -1,22 +1,53 @@
-import {Stack, StackProps, StageProps} from "aws-cdk-lib";
+import {Stack, StackProps} from "aws-cdk-lib";
 import {Construct} from "constructs";
-import {Bucket} from "aws-cdk-lib/aws-s3";
-import {ARecord, HostedZone, HostedZoneAttributes, RecordTarget} from "aws-cdk-lib/aws-route53";
+import {HostedZone, HostedZoneAttributes} from "aws-cdk-lib/aws-route53";
+import {Credentials, DatabaseInstance, DatabaseInstanceEngine, PostgresEngineVersion} from "aws-cdk-lib/aws-rds";
+import {InstanceClass, InstanceSize, InstanceType, Port, SubnetType, Vpc} from "aws-cdk-lib/aws-ec2";
 
 export interface IAppStackProps extends StackProps {
   zoneAttrs: HostedZoneAttributes
 }
 
 export class AppStack extends Stack {
+
+  public vpcName: 'my-cortex'
+
   constructor(scope: Construct, id: string, props: IAppStackProps) {
     super(scope, id, props);
 
     const zone = HostedZone.fromHostedZoneAttributes(this, 'HostedZone', props.zoneAttrs)
-    new ARecord(this, 'ARecord', {
-      zone,
-      target: RecordTarget.fromIpAddresses('4.4.4.4'),
 
-
+    const vpc = new Vpc(this, 'Vpc', {
+      vpcName: this.vpcName,
+      cidr: "10.0.0.0/16",
+      natGateways: 1,
+      subnetConfiguration: [
+        {
+          name: "public",
+          subnetType: SubnetType.PUBLIC,
+          cidrMask: 24
+        },
+        {
+          name: "private",
+          subnetType: SubnetType.PRIVATE_WITH_NAT,
+          cidrMask: 24
+        }
+      ],
     })
+
+    const dbInstance = new DatabaseInstance(this, 'DatabaseInstance', {
+      instanceIdentifier: 'cortex-pg',
+      vpc,
+      engine: DatabaseInstanceEngine.postgres({version: PostgresEngineVersion.VER_14_2}),
+      instanceType: InstanceType.of(InstanceClass.BURSTABLE3, InstanceSize.MICRO),
+      credentials: Credentials.fromUsername('postgres', {secretName: `pg-secret-for-${props.stackName}`}),
+      vpcSubnets: {subnetType: SubnetType.PUBLIC},
+      allocatedStorage: 20,
+      maxAllocatedStorage: 40,
+      multiAz: false,
+    })
+    dbInstance.connections.allowFromAnyIpv4(Port.tcp(dbInstance.instanceEndpoint.port))
+
+
   }
 }
